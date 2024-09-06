b0d58aa31182ffd3d13ad5acfa7779843f575b97
Akash Kumar <meakash7902@gmail.com>
2024-09-06 12:00:00 +0000

    optimize reconnection delay
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,11 +1,12 @@
 import {io} from 'socket.io-client';
 
 export const initSocket = async () =>{
-    const options = {
-        'force new connection': true,
-        reconnectionAttempts : 'Infinity',
-        timeout: 10000,
-        transports: ['websocket'],
-    };
+    const socket = io({
+        reconnection: true,               // Enable automatic reconnection
+        reconnectionAttempts: 5,          // Number of retries
+        reconnectionDelay: 50,            // Initial delay (in ms)
+        reconnectionDelayMax: 250,        // Max delay (in ms)
+        timeout: 5000                     // Timeout for connection attempt
+      });      
     return io(process.env.REACT_APP_BACKEND_URL, options);
 }
